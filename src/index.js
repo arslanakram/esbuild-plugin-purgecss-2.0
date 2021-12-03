d2cadbaecd6392b0275c274918407f43a52ef54d
Arslan Akram <8457398+arslanakram@users.noreply.github.com>
2021-12-03 15:54:12 +0500

    comma formatting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 module.exports = function purgecssPlugin(options) {
     return {
-        name: "purgecss",
+        name: 'purgecss',
         setup(build) {
             if (!build.initialOptions.metafile) {
-                throw new Error("You should set metafile true to use this plugin.");
+                throw new Error('You should set metafile true to use this plugin.');
             }
-            const { PurgeCSS } = require("purgecss");
-            const path = require("path");
-            const fs = require("fs");
+            const { PurgeCSS } = require('purgecss');
+            const path = require('path');
+            const fs = require('fs');
 
             build.onEnd(async (args) => {
                 const outputKeys = Object.keys(args.metafile.outputs);
                 const genFilter = (postfix) => (k) => k.endsWith(postfix);
 
-                const content = outputKeys.filter(genFilter(".js"));
-                const css = outputKeys.filter(genFilter(".css"));
+                const content = outputKeys.filter(genFilter('.js'));
+                const css = outputKeys.filter(genFilter('.css'));
                 const opts = options ? options : {};
 
                 const res = await new PurgeCSS().purge({ ...opts, content, css });
